f2d4b63528b3c299274298893e3a20166d050e66
Joseph2006t <laizatellojoseph@gmail.com>
2024-12-09 03:41:51 -0500

    primer commit

---
+++ ejercicioPrueba.py
@@ -0,0 +1,14 @@
+#importamos la libreria para usar Tkinter
+import tkinter as tk
+#Crea la ventana
+ventana = tk.Tk()
+
+#Establecemos el tamaño de la ventana
+ventana.geometry('600x400')
+
+#Establecemos el título de la ventana
+ventana.title('Nueva Ventana')
+
+#Este comando siempre debe ir al final del codigo
+# por que este comando es para ejecutar la ventana
+ventana.mainloop()
